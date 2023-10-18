44ad91ad2fcda635193a8b3fd8c6342d6487152e
utipn <utipn@yahoo.com>
2023-10-17 22:05:35 -0600

    changes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
 module "s3" {
-    source = "git::git@github.com:Utipn/S3_backend_repo_me.git//s3-module?ref=v1.1.1"
+    source = "git::git@github.com:Utipn/S3_backend_repo_me.git//s3-module?ref=v1.1.0"
   
 }
